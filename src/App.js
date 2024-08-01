e299c4d71d6984247151856794b075db1056f9cf
SergeyIvaschennko <boss.awdr@mail.ru>
2024-08-01 18:17:25 +0300

    Init test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          King of scars
         </a>
       </header>
     </div>
