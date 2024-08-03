51b50597622edc95cfde283514e605781d5bf8c4
SergeyIvaschennko <boss.awdr@mail.ru>
2024-08-04 00:29:33 +0300

    The cover is done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-        <img src="https://i.pinimg.com/564x/e5/dc/f0/e5dcf0568fb397ded65581f22fd8c96d.jpg"/>
-      </header>
+        <Home/>
     </div>
   );
 }
---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css'; // Подключаем CSS стили
+import library_cover from "../../pics/library.jpg"
+const Home = () => {
+    return (
+        <div className="home-container">
+            <div className="image-container">
+                <img src={library_cover} alt="Main" className="background-image" />
+                <div className="overlay"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
