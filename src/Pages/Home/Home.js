a1079dcccc40c98881bc90ed0fbe9af6f2296e51
SergeyIvaschennko <boss.awdr@mail.ru>
2024-08-04 16:48:30 +0300

    Hidden navbar is made

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Home.css'; // Подключаем CSS стили
 import Navbar from "../../Components/Navbar/Navbar";
-import library_cover from "../../Pics/library.jpg"
+import library_cover from "../../Pics/library.jpg";
+import HiddenNavbar from "../../Components/Hidden navbar/Hidden-Navbar";
+import '../../Components/Hidden navbar/Hidden-Navbar.css'
+
 const Home = () => {
+    const [showHiddenNavbar, setShowHiddenNavbar] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const coverElement = document.querySelector('.image-container');
+            if (coverElement) {
+                const coverHeight = coverElement.clientHeight;
+                if (window.scrollY > coverHeight) {
+                    setShowHiddenNavbar(true);
+                } else {
+                    setShowHiddenNavbar(false);
+                }
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
+
     return (
         <div className="home-container">
             <div className="image-container">
                 <img src={library_cover} alt="Main" className="background-image" />
                 <div className="overlay">
-                    <Navbar/>
+                    <Navbar />
                 </div>
             </div>
+            {showHiddenNavbar && (
+                <div className="hidden-navbar">
+                    <HiddenNavbar/>
+                    выавы
+                </div>
+            )}
         </div>
-
     );
 };
 
